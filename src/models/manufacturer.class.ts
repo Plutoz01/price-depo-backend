1c29a34c8e28d1591395765db2e910eafb6e6f09
László Csatlós <plutoz01@gmail.com>
2017-12-15 15:53:04 +0100

    Add manufacturer ctrl + repo

---
+++ src/models/manufacturer.class.ts
@@ -0,0 +1,13 @@
+import { JsonProperty, Required } from 'ts-express-decorators';
+import { Identifiable } from './identifiable.interface';
+
+export class Manufacturer implements Identifiable<string> {
+	@JsonProperty()
+	id?: string;
+
+	@Required()
+	name: string;
+
+	@Required()
+	country: string;
+}
